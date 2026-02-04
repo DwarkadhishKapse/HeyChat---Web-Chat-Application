4ea536482a3b5ea4d8909e5f7ca529162df75ab0
Dwarkadhish Kapse <dwarkadhishkapse@gmail.com>
2026-02-04 16:41:35 +0530

    update origin in app js

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL?.split(","),
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
